1a4eec69cb35a2c6747b92f891da09aad0e46687
Yader Alvarez <yader877584@gmail.com>
2025-10-01 09:38:12 -0500

    nada

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
 import Home from './pages/home'
 
-
+//a
 function App(): React.JSX.Element {
   //const ipcHandle = (): void => window.electron.ipcRenderer.send('ping')
 
