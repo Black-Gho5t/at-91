57bac23ee2bc6866da0ce5ec242aa3c9983dd828
yader <yader877584@gmail.com>
2025-10-22 14:46:01 -0500

    casi nada, solo edicion de stylo de login

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import React from 'react'
 import Home from './pages/home'
-import LoginPage from "./components/loginPage";
+import LoginPage from "./components/login/loginPage";
 
 //a
 function App(): React.JSX.Element {
---
+++ src/renderer/src/components/login/newUser.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import "../../assets/loginPage.css"
+
+export default function NewUser(): React.JSX.Element {
+  return (
+    <div className="container-login">
+      <form className="form">
+        <p className="form-title">Iniciar sesion</p>
+        <div className="input-container">
+          <input type="email" placeholder="Email"></input>
+          <span>
+          </span>
+        </div>
+        <div className="input-container">
+          <input type="password" placeholder="Contraseña"></input>
+        </div>
+        <button type="submit" className="submit">
+          Iniciar
+        </button>
+
+        <p className="signup-link">
+          No tienes cuenta?
+          <a href=""> Crear cuenta</a>
+        </p>
+      </form>
+    </div>
+  )
+}
